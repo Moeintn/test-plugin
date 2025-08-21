a85c7b3ea3a9aa3937ef94bf9e02a774b2a89e99
Moeintn <moeintnl0@gmail.com>
2025-08-21 14:22:10 +0330

    Initial commit - پروژه PHP تستی

---
+++ test-plugin.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: woogram
+ * Description: <strong>دستیار تلگرامی</strong> برای فروشگاه شما
+ * Version: 1.0
+ * Author: MoeinTnl
+ * plugin uri: https://moeintn.ir
+ * requires php: 7.4
+ */
